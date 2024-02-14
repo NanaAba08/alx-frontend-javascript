87d52e1bdae0a9756f3b88c925d3145bcc411449
NanaAba08 <williamsnanaaba@gmail.com>
2024-02-14 14:10:45 -0800

    task_0 main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,4 +1,3 @@
-// Define the interface Student
 interface Student {
     firstName: string;
     lastName: string;
@@ -6,7 +5,6 @@ interface Student {
     location: string;
 }
 
-// Create two students
 const student1: Student = {
     firstName: "John",
     lastName: "Doe",
@@ -21,19 +19,19 @@ const student2: Student = {
     location: "Los Angeles"
 };
 
-// Create an array named studentsList containing the two variables
 const studentsList: Student[] = [student1, student2];
 
-// Render a table and append a new row for each student in the array
-const table = document.createElement('table');
-
-studentsList.forEach(student => {
-    const row = table.insertRow();
-    const cell1 = row.insertCell();
-    const cell2 = row.insertCell();
-    
-    cell1.textContent = student.firstName;
-    cell2.textContent = student.location;
+function renderTable() {
+    const table = document.createElement('table');
+    studentsList.forEach(student => {
+	const row = table.insertRow();
+	const cell1 = row.insertCell();
+	const cell2 = row.insertCell();
+	cell1.textContent = student.firstName;
+	cell2.textContent = student.location;
 });
 
 document.body.appendChild(table);
+}
+
+renderTable(studentsList);
